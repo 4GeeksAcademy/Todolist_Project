38eee963d20939b81d7d5094dd9022b6d29a3d27
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-05-29 19:58:24 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	const handleDelete = (index) => {
+		const newTodos = todos.filter((_, i) => i !== index);
+		setTodos(newTodos);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todoList">
+			<h1 className="title">To do</h1>
+			<div className="tasks">
+				<ul className="list-group">
+					<li className="list-group-item">
+					<input 
+					    type="text" 
+						onChange={(event) => setInputValue(event.target.value)} 
+						value={inputValue}
+						onKeyPress={(event) => {
+							if (event.key === 'Enter' && inputValue.trim() !== "") {
+								setTodos([...todos, inputValue]);
+								setInputValue("");
+							}
+						}}
+						placeholder={todos.length === 0 ? "No tasks, add tasks" : "What needs to be done?"}
+					/>
+					</li>
+					{todos.map((todo, index) =>(
+						<li key={index} className="list-group-item">
+							{todo}
+							<div className="trash-icon-container">
+								<FontAwesomeIcon icon={faTrash} className="trash-icon" onClick={() => handleDelete(index)} />
+							</div>
+						</li>
+					))}	
+				</ul>
+				<div className="bottom">{todos.length} tasks</div>
+			</div>
 		</div>
 	);
 };
